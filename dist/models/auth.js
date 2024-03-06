0fb817aca1fbc74c39771a52363621a08731586a
joseph.aguilar <joseph.aguilar@metrobank.com.ph>
2024-03-06 14:48:17 +0800

    new js

---
+++ dist/models/auth.js
@@ -0,0 +1,37 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// src/models/User.ts
+const sequelize_1 = require("sequelize");
+const db_1 = __importDefault(require("../config/db"));
+class Auth extends sequelize_1.Model {
+}
+Auth.init({
+    mobile: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: false,
+        unique: false,
+    },
+    password: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: false,
+    },
+    role: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: false,
+    },
+    isVerified: {
+        type: sequelize_1.DataTypes.BOOLEAN,
+        allowNull: false,
+    },
+}, {
+    sequelize: db_1.default,
+    modelName: "Auth",
+});
+exports.default = Auth;
