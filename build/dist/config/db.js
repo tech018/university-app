3f447e6d5f4bd59dc5a8aad98dcd7f8978bf0389
joseph.aguilar <joseph.aguilar@metrobank.com.ph>
2024-03-06 14:54:48 +0800

    new commit

--- build/dist/config/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const index_1 = __importDefault(require("../config/index"));
-const sequelize = new sequelize_1.Sequelize(index_1.default.database);
-exports.default = sequelize;
